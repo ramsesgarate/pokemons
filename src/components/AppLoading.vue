955ef6729e14a6967897fc6645dcec6e18b0097a
Ramses G <rsgarate@gmail.com>
2020-10-04 01:11:49 -0300

    feat: loading is created

---
+++ src/components/AppLoading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="h-screen flex items-center justify-center absolute inset-0 z-50 bg-gray-light-3 transition-all duration-200"
+    v-if="value"
+  >
+    <img src="@/assets/img/loader.svg" alt="" class="animate-pulse" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Boolean
+  }
+}
+</script>
