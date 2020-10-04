71233240fc46632c68f9ad8c88058f27d535f49d
Ramses G <rsgarate@gmail.com>
2020-10-04 01:12:51 -0300

    feat: the button component and start button are created to save the favorite

---
+++ src/components/AppButton.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <router-link
+      type="button"
+      :to="to"
+      class="button focus:outline-none"
+      :class="[
+        this.disabled ? 'bg-gray' : 'bg-red',
+        this.disabled ? 'hover:bg-gray-dark-1' : 'hover:bg-red-dark-1',
+        this.classes
+      ]"
+      v-on="$listeners"
+      v-if="to"
+    >
+      <span class="v-btn__content text-white flex justify-center">
+        <app-icon :name="icon" v-if="icon" class="mr-4" />
+        <slot></slot>
+      </span>
+    </router-link>
+    <button
+      type="button"
+      class="button focus:outline-none"
+      :class="[
+        this.disabled ? 'bg-gray' : 'bg-red',
+        this.disabled ? 'hover:bg-gray-dark-1' : 'hover:bg-red-dark-1',
+        this.classes
+      ]"
+      v-on="$listeners"
+      v-else
+    >
+      <span class="v-btn__content text-white flex justify-center">
+        <app-icon :name="icon" v-if="icon" class="mr-4" />
+        <slot></slot>
+      </span>
+    </button>
+  </div>
+</template>
+
+<script>
+import AppIcon from './AppIcon'
+
+export default {
+  props: {
+    icon: String,
+    to: {
+      type: [String, Object]
+    },
+    width: {
+      default: null,
+      type: String
+    },
+    classes: {
+      default: null,
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  components: {
+    AppIcon
+  }
+}
+</script>
+
+<style>
+.button {
+  @apply py-3 px-5 rounded-full transition duration-200 mx-2;
+}
+</style>
---
+++ src/components/AppButtonStart.vue
@@ -0,0 +1,25 @@
+<template>
+  <span
+    class="relative rounded-full bg-gray-light-3 h-p-44 w-p-44 cursor-pointer"
+    v-on="$listeners"
+  >
+    <app-icon
+      name="start"
+      class="absolute top-50-26/2 right-11"
+      :color="fav ? '#ECA539' : '#BFBFBF'"
+    />
+  </span>
+</template>
+
+<script>
+import AppIcon from '@/components/AppIcon'
+
+export default {
+  props: {
+    fav: Boolean
+  },
+  components: {
+    AppIcon
+  }
+}
+</script>
