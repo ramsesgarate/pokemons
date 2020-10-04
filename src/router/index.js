5a958e9ef68d3a8646a10488f8b94e2c46b4579d
Ramses G <rsgarate@gmail.com>
2020-10-04 01:06:55 -0300

    feat: the routes to be used in the project are added

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Welcome from '../views/Welcome.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'welcome',
+    component: Welcome
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: '/pokemons/',
+    name: 'pokemons',
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+      import(/* webpackChunkName: "pokemons" */ '../views/Pokemons.vue'),
+    children: [
+      {
+        path: ':id',
+        name: 'pokemon',
+        component: () =>
+          import(/* webpackChunkName: "pokemon" */ '../views/Pokemon.vue')
+      }
+    ]
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
