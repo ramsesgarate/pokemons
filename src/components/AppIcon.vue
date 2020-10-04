2a1d743e26fa85a9039d2fb45a67a20ad45ed55f
Ramses G <rsgarate@gmail.com>
2020-10-04 00:58:14 -0300

    feat: a svg file is created to host all the icons and a component to access them

---
+++ src/components/AppIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg :width="width" :height="height" :fill="color" v-on="$listeners">
+    <use v-bind="{ 'xlink:href': '/icons.svg#' + name }" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    name: String,
+    color: {
+      type: String,
+      default: 'white'
+    },
+    width: {
+      type: [Number, String],
+      default: 22
+    },
+    height: {
+      type: [Number, String],
+      default: 22
+    }
+  }
+}
+</script>
