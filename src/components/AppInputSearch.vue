9ea26ba26c1bc89181710f8cdfea60b4e5523df3
Ramses G <rsgarate@gmail.com>
2020-10-04 01:14:25 -0300

    feat: An input search is created to search for the pokemon

---
+++ src/components/AppInputSearch.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex flex-col mb-8 max-w-xl mx-auto">
+    <div class="relative ">
+      <div class="absolute flex h-full w-10 z-10">
+        <div class="flex items-center justify-center w-full">
+          <app-icon name="search" />
+        </div>
+      </div>
+
+      <input
+        id="name"
+        name="name"
+        type="text"
+        placeholder="Search"
+        class="relative w-full rounded placeholder-gray-400 focus:outline-none py-2 pr-2 pl-12"
+        :value="value"
+        @input="updateValue"
+        autocomplete="off"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppIcon from '@/components/AppIcon'
+
+export default {
+  inheritAttrs: false,
+  props: {
+    value: String
+  },
+  components: {
+    AppIcon
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
